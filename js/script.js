cbcef196750743ea03415c943dcbd2b1db9c8ebc
yana-toder <112866898+yana-toder@users.noreply.github.com>
2022-11-27 13:39:19 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+'use strict'
+
+console.log('Hello');
+console.log('Вчимо');
+console.log('JS');
+console.log('Вчимо' + ' JS');
+
+let num = 5;
+console.log(num);
+
+//Змінні
+let colorEyes;
+let user;
+let userName;
+
+userName = 'Вася';
+user = 'Вася';
+console.log(user);
+
+//========
+let userAge = '58';
+console.log(userAge);
+console.log(typeof userAge);
+
+userAge = Number(userAge);
+
+console.log(userAge);
+console.log(typeof userAge);
+
+let userHeight = 145 / 0;
+console.log(userHeight);
+
+let userNumber;
+console.log(userNumber);
+
+let userSize = "45" / "8";
+console.log(typeof userSize);
